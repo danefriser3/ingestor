86eed20200e13da5798e6ce99fd943ec90abf586
Daniele Parisi <parisi_daniele@hotmail.it>
2026-01-28 15:04:05 +0000

    Update database connection string to use sslmode=verify-full

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 import pkg from "pg";
 const { Pool } = pkg;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://default:4p5hDVCAuSin@ep-snowy-scene-a40k3p50-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=require&channel_binding=require';
+const connectionString = process.env.DATABASE_URL || 'postgresql://default:4p5hDVCAuSin@ep-snowy-scene-a40k3p50-pooler.us-east-1.aws.neon.tech/verceldb?sslmode=verify-full&channel_binding=require';
 
 const pool = new Pool({
   connectionString,
